d9cf680a8a3a47ffce6f19a513af40b59f2c8451
wangyongy <wangyongy@yonyou.com>
2020-05-02 15:19:05 +0800

    lombok注解修改

--- src/main/java/com/example/cache/entity/Student.java
+++ src/main/java/com/example/cache/entity/Student.java
@@ -1,6 +1,9 @@
 package com.example.cache.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -11,6 +14,9 @@ import java.io.Serializable;
  * @since 2020-04-30 10:15:54
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
 public class Student implements Serializable {
     private static final long serialVersionUID = -41269457751686094L;
     
@@ -26,7 +32,4 @@ public class Student implements Serializable {
     
     private String address;
 
-    public Student() {
-        super();
-    }
 }
